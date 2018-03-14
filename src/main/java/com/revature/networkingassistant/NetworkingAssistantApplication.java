548bf90368e7a712f1cabf111eb272f6060b86e6
Sawyer Novak <sawyern@me.com>
2018-03-14 11:27:08 -0400

    spring boot

---
+++ src/main/java/com/revature/networkingassistant/NetworkingAssistantApplication.java
@@ -0,0 +1,12 @@
+package com.revature.networkingassistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetworkingAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetworkingAssistantApplication.class, args);
+	}
+}
