dd6340357439b65378e41df1df32aeb066758e0c
Sawyer Novak <sawyern@me.com>
2018-03-14 21:55:44 -0400

    register,login,logout api functioning tested locally, needs integration tests w/ mockito

--- src/main/java/com/revature/networkingassistant/controllers/AccountController/RegisterController.java
+++ src/main/java/com/revature/networkingassistant/controllers/AccountController/RegisterController.java
@@ -6,6 +6,7 @@ import com.revature.networkingassistant.repositories.AccountRepo;
 import com.revature.networkingassistant.repositories.SessionTokenRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
