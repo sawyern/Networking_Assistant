9dd9dd3bc62991570092f3d96843579fa8650fdb
Son Phung <son.phung.1@gmail.com>
2018-03-16 16:59:21 -0500

    attempt to make register account test work, most changes reverted

--- src/main/java/com/revature/networkingassistant/controllers/AccountController/RegisterController.java
+++ src/main/java/com/revature/networkingassistant/controllers/AccountController/RegisterController.java
@@ -14,13 +14,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RegisterController {
-    private AccountRepo accountRepo;
-    public RegisterController() { }
 
     @Autowired
-    public RegisterController(AccountRepo accountRepo) {
-        this.accountRepo = accountRepo;
-    }
+    private AccountRepo accountRepo;
 
     //checks most emails, stole it from the internet
     private static final String emailRegex = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
--- src/test/java/com/revature/networkingassistant/controllers/AccountController/RegisterControllerTest.java
+++ src/test/java/com/revature/networkingassistant/controllers/AccountController/RegisterControllerTest.java
@@ -1,13 +1,32 @@
 package com.revature.networkingassistant.controllers.AccountController;
 
+import com.revature.networkingassistant.beans.Account;
+import com.revature.networkingassistant.controllers.DTO.JsonRequestBody;
+import com.revature.networkingassistant.controllers.EventsController.CreateEventController;
+import com.revature.networkingassistant.repositories.AccountRepo;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.transaction.Transactional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@RunWith(SpringRunner.class)
+@WebMvcTest(RegisterController.class)
+@Transactional
 class RegisterControllerTest {
 
     @Test
-    void registerAccount() {
-
+    void registerAccountTest() {
+        RegisterController controller = new RegisterController();
+        byte[] test = "test".getBytes();
+        Account account = new Account("test@test.test", "test", "test", "test", "test", test);
+        JsonRequestBody<Account> requestBody = new JsonRequestBody<>();
+        requestBody.setObject(account);
+        Account testAccount = controller.registerAccount(requestBody);
+        assertNotNull(testAccount);
     }
 }
