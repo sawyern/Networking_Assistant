e361e797a9ba66185fa6f32ab9cd913f496235cd
Maxrimus <joe.p.tursi@gmail.com>
2018-03-21 22:09:39 -0400

    Created button to take you the the New Event page from the Event Dashboard page.

--- front-end/src/app/pages/event-dashboard/event-dashboard/event-dashboard.component.ts
+++ front-end/src/app/pages/event-dashboard/event-dashboard/event-dashboard.component.ts
@@ -1,5 +1,6 @@
 import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {EventComponent} from "../event/event.component";
+import {GoToService} from "../../../_services/go-to/go-to.service";
 
 @Component({
   selector: 'app-event-dashboard',
@@ -10,7 +11,7 @@ export class EventDashboardComponent implements OnInit {
 
   @ViewChild(EventComponent) event:EventComponent;
 
-  constructor() { }
+  constructor(private goToService:GoToService) { }
 
   updateEvent(e){
     this.event.setEvent(e);
