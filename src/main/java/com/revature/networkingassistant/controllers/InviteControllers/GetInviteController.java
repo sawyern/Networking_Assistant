e36683ab2ca0e3b0361b0efacedc964a971ebc13
Sawyer Novak <sawyern@me.com>
2018-03-22 20:13:34 -0400

    fixed merge

--- src/main/java/com/revature/networkingassistant/controllers/InviteControllers/GetInviteController.java
+++ src/main/java/com/revature/networkingassistant/controllers/InviteControllers/GetInviteController.java
@@ -1,31 +1,36 @@
 package com.revature.networkingassistant.controllers.InviteControllers;
 
+import com.revature.networkingassistant.ControllerUtil;
 import com.revature.networkingassistant.beans.Invite;
 import com.revature.networkingassistant.controllers.DTO.JsonRequestBody;
 import com.revature.networkingassistant.services.InviteServices.GetInviteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
 @RestController
+@CrossOrigin(origins = ControllerUtil.CORS_ALLOW)
 public class GetInviteController {
 
+    private GetInviteService getInviteService;
+
+    public GetInviteController() {}
+
     @Autowired
-    private GetInviteService inviteService;
+    public GetInviteController(GetInviteService getInviteService) {
+        this.getInviteService = getInviteService;
+    }
 
     @RequestMapping(path = "/api/invites/getSentInvites", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<ArrayList<Invite>> getSentInvites(@RequestBody JsonRequestBody<Invite> requestBody) {
-        return inviteService.getSentInvites(requestBody);
+        return getInviteService.getSentInvites(requestBody);
     }
 
     @RequestMapping(path = "/api/invites/getReceivedInvites", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<ArrayList<Invite>> getReceivedInvites(@RequestBody JsonRequestBody<Invite> requestBody) {
-        return inviteService.getRecievedInvites(requestBody);
+        return getInviteService.getRecievedInvites(requestBody);
     }
 }
--- src/main/java/com/revature/networkingassistant/services/InviteServices/InviteService.java
+++ src/main/java/com/revature/networkingassistant/services/InviteServices/InviteService.java
@@ -12,7 +12,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @Service
 public class InviteService {
