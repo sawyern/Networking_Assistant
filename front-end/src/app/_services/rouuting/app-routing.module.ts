767a593cb2d02bcb2f5c6f5bc7031072cdfae62c
Sawyer Novak <sawyern@me.com>
2018-03-24 17:55:05 -0400

    Merge branch 'dev' of https://github.com/sawyern/Networking_Assistant into dev
    # Conflicts:
    #	front-end/src/app/_services/rouuting/app-routing.module.ts
    #	front-end/src/app/pages/event-dashboard/my-events/my-events.component.ts

--- front-end/src/app/_services/rouuting/app-routing.module.ts
+++ front-end/src/app/_services/rouuting/app-routing.module.ts
@@ -1,18 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from '../../pages/login/login/login.component';
-// import { RegisterComponent } from "./register/register.component";
 import { AuthGuard} from '../../_guards/auth.guard';
 import {HomeComponent} from '../../pages/home/home.component';
-import {RegisterComponent} from "../../pages/login/register/register.component";
 import {ProfileComponent} from "../../pages/profile/profile.component";
 import {EventDashboardComponent} from "../../pages/event-dashboard/event-dashboard/event-dashboard.component";
 import {NewEventComponent} from "../../pages/new-event/new-event/new-event.component";
 import {LoginPageComponent} from "../../pages/login/login-page/login-page.component";
 import {StarComponent} from "../../pages/profile/star/star.component";
 import {MyEventsComponent} from "../../pages/event-dashboard/my-events/my-events.component";
-import { StarredComponent } from '../../pages/profile/starred/starred.component';
+import {StarredComponent} from "../../pages/profile/starred/starred.component";
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
@@ -24,7 +21,7 @@ const appRoutes: Routes = [
       { path: 'profile/:accountId/starred', component: StarComponent },
       { path: 'event/dashboard', component: EventDashboardComponent },
       { path: 'event/new', component: NewEventComponent },
-      { path: 'account/starred', component: StarredComponent}
+      { path: 'account/starred', component: StarredComponent},
       { path: 'event/my-events', component: MyEventsComponent}
     ]
   },
